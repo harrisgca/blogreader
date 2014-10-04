c7262ce9795d269d4a63e20bf667ca9a92698869
Glenn <harris.glenn.jr@gmail.com>
2014-10-04 13:52:50 -0700

    wiping sys

--- app/src/main/java/net/ghdev/blogreader/MainListActivity.java
+++ app/src/main/java/net/ghdev/blogreader/MainListActivity.java
@@ -1,35 +1,31 @@
 package net.ghdev.blogreader;
 
 import android.app.ListActivity;
+import android.content.res.Resources;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
+import android.widget.Toast;
 
 
 public class MainListActivity extends ListActivity {
 
-    protected String[] mAndroidNames = {
-            "Android beta",
-            "Android 1.0",
-            "Android 1.1",
-            "Cupcake",
-            "Donut",
-            "Eclair",
-            "Gingerbread",
-            "Honeycomb",
-            "Ice Cream Sandwich",
-            "Jelly Bean"
-    };
+    protected String[] mAndroidNames;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_list);
 
+        Resources resources = getResources();
+        mAndroidNames = resources.getStringArray(R.array.android_names);
+
         ArrayAdapter <String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mAndroidNames);
         setListAdapter(adapter);
+
+        //Toast.makeText(this, getString(R.string.no_items), Toast.LENGTH_LONG).show();
     }
 
 
